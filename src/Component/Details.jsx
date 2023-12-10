e40b86169105b1926ce15b53bf96771dc7ae244f
gulanahacizada <gulanahacizada@gmail.com>
2023-12-10 10:44:00 -0800

    updatee

---
+++ src/Component/Details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter, Route, Link, Routes, NavLink } from "react-router-dom";
+function Details() {
+  return (
+    <div>
+      detail
+    </div>
+  )
+}
+
+export default Details
